537ba2c21513ce7cbc58b43d5a0721af11ee19a0
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-09 17:15:50 +0100

    basic interface created, needs revising

--- public/js/data.js
+++ public/js/data.js
@@ -7,23 +7,20 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 // DOM nodes
 const numCont = document.querySelector('.container');
+const userInputsCont = document.querySelector('.user-inputs');
+const userInputs = document.querySelectorAll('input');
+const checkBtn = document.querySelector('button');
+const resultCont = document.querySelector('.result');
 
 // global variables
 const randomNumbers = [];
 const userNumbers = [];
 const rightNumbers = [];
 
-// setTimeout(function() {
-// 	for (let i = 0; i < 5; i++) {
-// 		const userInput = parseInt(prompt('Inserisci il numero che ricordi di aver visualizzato'));
-// 		userNumbers.push(userInput);
-// 		if (randomNumbers.includes(userInput)) {
-// 			rightNumbers.push(userInput);
-// 		}
-// 	}
-// 	console.log(userNumbers);
-// 	console.log(rightNumbers);
-// }, 30000)
+setTimeout(function() {
+	numCont.classList.add('hidden');
+	userInputsCont.classList.remove('hidden');
+}, 10000);
 
 // generates 5 different random numbers to be displayed on the page
 while (randomNumbers.length < 5) {
@@ -39,4 +36,19 @@ console.log(randomNumbers);
 // displays randomNumbers on page
 randomNumbers.forEach((number) => {
 	numCont.innerHTML += `<div class="number-container">${number}</div>`;
-})
+});
+
+checkBtn.addEventListener('click', function() {
+	userInputs.forEach(function(n) {
+		userNumbers.push(Number(n.value));
+		if (randomNumbers.includes(Number(n.value))) {
+			rightNumbers.push(Number(n.value));
+		}
+	});
+	console.log(rightNumbers);
+	userInputsCont.classList.add('hidden');
+	resultCont.classList.remove('hidden');
+	rightNumbers.forEach((number) => {
+		resultCont.innerHTML += `<div class="number-container">${number}</div>`;
+	});
+});
