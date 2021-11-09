1e565120019d5fc64f25d2896416b5dfe69e4139
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-09 17:46:04 +0100

    interface completed, starting revising

--- public/js/data.js
+++ public/js/data.js
@@ -6,11 +6,13 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 */
 
 // DOM nodes
-const numCont = document.querySelector('.container');
+const numCont = document.getElementById('random-numbers');
+const gameCont = document.querySelector('.container');
 const userInputsCont = document.querySelector('.user-inputs');
 const userInputs = document.querySelectorAll('input');
 const checkBtn = document.querySelector('button');
 const resultCont = document.querySelector('.result');
+const finalResult = document.querySelector('.user-result');
 
 // global variables
 const randomNumbers = [];
@@ -18,7 +20,7 @@ const userNumbers = [];
 const rightNumbers = [];
 
 setTimeout(function() {
-	numCont.classList.add('hidden');
+	gameCont.classList.add('hidden');
 	userInputsCont.classList.remove('hidden');
 }, 10000);
 
@@ -48,7 +50,16 @@ checkBtn.addEventListener('click', function() {
 	console.log(rightNumbers);
 	userInputsCont.classList.add('hidden');
 	resultCont.classList.remove('hidden');
-	rightNumbers.forEach((number) => {
-		resultCont.innerHTML += `<div class="number-container">${number}</div>`;
-	});
+	if (rightNumbers.length > 0) {
+		const userResult = document.createElement('div');
+		userResult.classList.add('user-result');
+		resultCont.innerHTML = `<h1>Di seguito i numeri giusti</h1>
+		`;
+		resultCont.append(userResult);
+		rightNumbers.forEach((number) => {
+			userResult.innerHTML += `<div class="number-container">${number}</div>`;
+		});
+	} else {
+		resultCont.innerHTML = `<h1>Spiacente, non hai azzeccato nemmeno un numero!</h1>`;
+	}
 });
